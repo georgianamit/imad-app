b743706cf3b9a11d9ea9b28448de749b2ab256a5
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-18 21:14:37 +0530

    compact code

--- server.js
+++ server.js
@@ -5,20 +5,77 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+  'article_one': {
+    title: "Article one | spider",
+    content: `
+          <p>
+              Hello, welcome to first article of this page. Its been so long we have contacted each other. And this is not
+              the way you can acheive the heights of success. You have to work smart. Everyone don't have same purpose in life.
+              So, stop pushing others to work according to you.
+          </p>
+          <p>
+              Hello, welcome to first article of this page. Its been so long we have contacted each other. And this is not
+              the way you can acheive the heights of success. You have to work smart. Everyone don't have same purpose in life.
+              So, stop pushing others to work according to you.
+          </p>
+          <p>
+              Hello, welcome to first article of this page. Its been so long we have contacted each other. And this is not
+              the way you can acheive the heights of success. You have to work smart. Everyone don't have same purpose in life.
+              So, stop pushing others to work according to you.
+          </p>
+    `
+  },
+  'article_two': {
+    title: "Article two | spider",
+    content: `
+          <p>
+              Hello, welcome to second article of this page. Its been so long we have contacted each other. And this is not
+              the way you can acheive the heights of success. You have to work smart. Everyone don't have same purpose in life.
+              So, stop pushing others to work according to you.
+          </p>
+
+    `
+  },
+  'article_three':{
+    title: "Article three | spider",
+    content: `
+          <p>
+              Hello, welcome to three article of this page. Its been so long we have contacted each other. And this is not
+              the way you can acheive the heights of success. You have to work smart. Everyone don't have same purpose in life.
+              So, stop pushing others to work according to you.
+          </p>
+
+    `
+  }
+}
+
+function createTemplate(data){
+  title = data.title;
+  content = data.content;
+  template = `
+  <html>
+  <head>
+      <title>
+          ${title}
+      </title>
+  </head>
+  <body>
+      <div>
+         ${content}
+      </div>
+  </body>
+</html>`
+return template;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article_one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
-});
-
-app.get('/article_two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
-});
-
-app.get('/article_three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
+app.get('/:articlename', function (req, res) {
+  var articlename = req.params.articlename;
+  res.send(createTemplate(articles[articlename]));
 });
 
 app.get('/ui/style.css', function (req, res) {
